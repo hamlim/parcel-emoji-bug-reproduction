9d49a7abd07464c632d5aea55ab06089a8873e1d
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-06-13 07:24:41 -0400

    Initial commit

---
+++ src/pages/_root.tsx
@@ -0,0 +1,29 @@
+import type { ReactNode } from "react";
+import "../styles.css";
+
+function themeCheck() {
+  let prefersDarkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  let preferred = prefersDarkModeQuery.matches ? "dark" : "light";
+  document.documentElement.classList.add(preferred);
+  prefersDarkModeQuery.addEventListener("change", (e) => {
+    let newPreferred = e.matches ? "dark" : "light";
+    document.documentElement.classList.remove(preferred);
+    document.documentElement.classList.add(newPreferred);
+    preferred = newPreferred;
+  });
+}
+
+export default function Root({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head suppressHydrationWarning>
+        <title>Parcel RSC Template</title>
+        <script
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+          dangerouslySetInnerHTML={{ __html: `(${themeCheck.toString()})()` }}
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
